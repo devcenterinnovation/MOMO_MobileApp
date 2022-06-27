46a76c94727322c693811cb1fd7a080f32453735
Samuel Adeniji <57541118+zammie01@users.noreply.github.com>
2022-06-27 13:16:31 +0100

    changes by samuel adeniji

---
+++ lib/services/api_docs.dart
@@ -0,0 +1,6 @@
+class ApiDocs {
+  static final BASE_URL = "https://momo-app-prdo9.ondigitalocean.app/";
+
+  static final LOGIN_URL = "auth/login";
+  static final SIGNUP_URL = "https://momo-app-prdo9.ondigitalocean.app/";
+}
