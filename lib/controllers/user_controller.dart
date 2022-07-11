e9efca1bc6296abe9af16d67924ac505ea5584ee
Samuel Adeniji <57541118+zammie01@users.noreply.github.com>
2022-07-11 19:14:24 +0100

    API integration.

---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,47 @@
+import 'package:get/get.dart';
+import 'package:momo/views/signup_screens/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/user_model.dart';
+
+
+class UserController extends GetxController {
+  String token = '';
+  String userId = '';
+  String referralId = '';
+  Profile? profile;
+  Wallet? wallet;
+
+
+
+  Profile? getProfile() => profile;
+
+  Wallet? getWallet() => wallet;
+
+
+
+  setUserDetail(data) {
+    Profile profile = Profile.fromJson(data);
+    this.profile = profile;
+  }
+
+
+  setUserWallet(data){
+    Wallet wallet = Wallet.fromJson(data);
+    this.wallet = wallet;
+  }
+
+  setToken(token) => this.token = token;
+
+
+  setUserId(userId) => this.userId = userId;
+
+  setReferralId(referralId) => this.referralId = referralId;
+
+  logOut() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.clear();
+    preferences.setBool("installed", true);
+    Get.offAll(() => const LoginPage());
+  }
+}
